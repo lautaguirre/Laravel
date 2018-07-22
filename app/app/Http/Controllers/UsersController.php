7a2768943423f683dba525d919fa30276688ef77
Lautaro Aguirre <32684961+lautaguirre@users.noreply.github.com>
2018-07-22 12:22:04 -0300

    Pagination

--- app/app/Http/Controllers/UsersController.php
+++ app/app/Http/Controllers/UsersController.php
@@ -7,11 +7,33 @@ use App\User;
 
 class UsersController extends Controller
 {
-    public function create() 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('id', 'ASC')->paginate(5);
+        return view('admin.users.index', ['users' => $users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
         return view('admin.users.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $user = new User($request->all());
@@ -20,5 +42,50 @@ class UsersController extends Controller
         $user->save();
 
         dd('Bien');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
---
+++ app/resources/views/admin/users/index.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.template.main')
+
+@section('title', 'Lista de Usuarios')
+
+@section('content')
+    <a href="{{ route('users.create') }}" class="btn btn-success">Crear Nuevo Usuario</a><hr>
+
+    <table class="table table-hover">
+        <thead>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Tipo</th>
+            <th>Accion</th>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        @if ($user->type == 'admin')
+                            <span class="badge badge-danger">{{ $user->type }}</span>
+                        @else
+                            <span class="badge badge-primary">{{ $user->type }}</span>
+                        @endif
+                    </td>
+                    <td><a href="" class="btn btn-danger"></a> <a href="" class="btn btn-warning"></a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {!! $users->render() !!}
+
+@endsection
