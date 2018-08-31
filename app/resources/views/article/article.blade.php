6e191fc29e3aa00592f3f29878a18f8927346af4
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2018-08-31 19:31:55 -0300

    added disqus

--- app/resources/views/article/article.blade.php
+++ app/resources/views/article/article.blade.php
@@ -11,6 +11,19 @@
   <div class="row">
     <div class="col-md-9">
       {!! $article->content !!}
+
+      <h3>Comentarios</h3>
+      <hr>
+      <div id="disqus_thread"></div>
+        <script>
+        (function() {
+        var d = document, s = d.createElement('script');
+        s.src = 'https://lautaguirre.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     </div>
     <div class="col-md-3 aside">
         @include('aside')
